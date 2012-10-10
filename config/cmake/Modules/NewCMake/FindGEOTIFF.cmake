ac852c19102585381f1762251716e62ebc86c0f7
Mario Ceresa <mrceresa@gmail.com>
2012-10-10 17:09:48 +0200

    Added include path for geotiff
    Signed-off-by: Mario Ceresa <mrceresa@gmail.com>

--- config/cmake/Modules/NewCMake/FindGEOTIFF.cmake
+++ config/cmake/Modules/NewCMake/FindGEOTIFF.cmake
@@ -8,6 +8,8 @@
 #  GEOTIFF_LIBRARY, where to find the GEOTIFF library.
 
 FIND_PATH(GEOTIFF_INCLUDE_DIR geotiff.h
+  /usr/local/include/libgeotiff
+  /usr/include/libgeotiff
   /usr/local/include
   /usr/include
 )
